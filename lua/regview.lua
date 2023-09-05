d7846fa33bb4b9b52a9a8ddeee137924cf271cff
Ryan Devenney <rwdevenney@gmail.com>
2023-09-05 16:36:49 -0400

    setup

--- lua/regview.lua
+++ lua/regview.lua
@@ -1,13 +1,20 @@
 local M = {}
 
 M.opts = {
-    cmd_key = "*"
+    cmd_key = "\""
 }
+
+local function run_regview()
+end
 
 ---sets up regview
 ---@param opts any
 M.setup = function(opts)
-    M.opts = vim.tbl_extend("force", M.opts, opts)
+    if type(opts) == "table" then
+        M.opts = vim.tbl_extend("force", M.opts, opts)
+    end
+
+    vim.keymap.set('n', M.opts.cmd_key, run_regview, {})
 end
 
 return M
